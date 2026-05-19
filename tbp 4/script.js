e43d44847caf6c78006b7f029e1701293f7ad2b9
osoagacblg <akbashuseyin3883@gmail.com>
2026-05-19 21:20:17 +0300

    Add files via upload

--- tbp 4/script.js
+++ tbp 4/script.js
@@ -1,6 +1,6 @@
 const agaclar = [
     {
-        ad: "Himalaya Sediri (Cedrus deodara)",
+        ad: 'Himalaya Sediri (<span class="bilimsel">Cedrus deodara</span>)',
         bilgi: "6-10 yaş aralığındadır. En ayırt edici özelliği, ana dalların uçlarının ve tepe sürgününün aşağıya doğru sarkık olmasıdır; bu ona ağlayan bir görünüm kazandırır.İğne yaprakları genellikle parlak yeşil veya hafif mavimsi-yeşil renktedir ve diğer sedir türlerine göre daha yumuşaktır.Işık isteği yüksek olan, derin ve iyi direnajlı toprakları seven bir ağaçtır.",
         resim: "images/agac4.jpg"
     },
@@ -13,7 +13,7 @@ function getId() {
 
 function agaciGoster(id) {
     const agac = agaclar[id - 1];
-    document.getElementById("agacAdi").innerText = agac.ad;
+    document.getElementById("agacAdi").innerHTML = agac.ad;
     document.getElementById("agacBilgi").innerText = agac.bilgi;
     document.getElementById("agacResim").src = agac.resim;
 }
